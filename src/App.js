6c8edec0975e1a2d045838c1c377e35579befb17
NikitaVysotskiy <fordf150svtraptor@gmail.com>
2018-01-08 17:25:05 +0200

    Added semantic-ui slider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Sidebar, Segment, Button, Menu, Icon, Header } from 'semantic-ui-react'
+
+import Content from './Content'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Your Balanceer</h2>
-        </div>
-      </div>
-    );
-  }
+
+    state = { visible: true };
+
+    toggleVisibility() {
+        this.setState({visible: !this.state.visible});
+    }
+
+    render() {
+        const { visible } = this.state;
+        return (
+            <div>
+                <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
+                <Sidebar.Pushable as={Segment}>
+                    <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
+                        <Menu.Item name='home'>
+                            <Icon name='home' />
+                            Home
+                        </Menu.Item>
+                        <Menu.Item name='gamepad'>
+                            <Icon name='gamepad' />
+                            Games
+                        </Menu.Item>
+                        <Menu.Item name='camera'>
+                            <Icon name='camera' />
+                            Channels
+                        </Menu.Item>
+                    </Sidebar>
+                    <Sidebar.Pusher>
+                        <Segment basic>
+                            <Header as='h3'>Application Content</Header>
+                            <Content/>
+                        </Segment>
+                    </Sidebar.Pusher>
+                </Sidebar.Pushable>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class Content extends Component {
+
+    render() {
+        return (
+            <div id='content'>
+
+            </div>
+        )
+    }
+}
+
+export default Content;
